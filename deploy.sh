c55bad46f8d8efdbc7d6abd0eaf1a53bf8789793
coba-yu <yuu.meiji.gkb@gmail.com>
2022-02-27 13:42:56 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,20 @@
 poetry export -f requirements.txt --output src/requirements.txt --without-hashes
 
-cd src || exit
+# https://cloud.google.com/sdk/gcloud/reference/auth/login
+gcloud auth login yuu.meiji.research@gmail.com
 
-set -eu
-function finally {
-  cd ..
-}
-trap finally EXIT
+PROJECT_ID='spry-pier-279201'
 
-zip -r ../ps5-crawler-src.zip . -x \*/__pycache__/\*
+# https://cloud.google.com/sdk/gcloud/reference/functions/deploy
+gcloud functions deploy \
+  ps5-crawler \
+  --region=asia-northeast1 \
+  --set-build-env-vars='LINE_NOTIFY_ACCESS_TOKEN=1rKprAC1sKGT49f8ZiwH6VsjLQFhbMjqnSFLnI577gz' \
+  --entry-point='main' \
+  --memory=512MB \
+  --runtime 'python39' \
+  --trigger-topic 'hourly' \
+  --service-account="${PROJECT_ID}@appspot.gserviceaccount.com" \
+  --source='src' \
+  --stage-bucket='cf-src' \
+  --project ${PROJECT_ID}
--- src/main.py
+++ src/main.py
@@ -1,22 +1,55 @@
 import os
 import re
 import shutil
+from pathlib import Path
+from stat import S_IXGRP, S_IXUSR
 from tempfile import TemporaryDirectory
+from traceback import format_exc
 
 from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 from line.notify import LineNotifier
 
 HIKARI_TV_URL = "https://shop.hikaritv.net/"
 
 
+def add_execute_permission(path: Path) -> None:
+    path.chmod(mode=path.stat().st_mode | S_IXUSR | S_IXGRP)
+
+
+def prepare_selenium_file(file_name: str, tmp_dir: str) -> str:
+    path = os.path.join(tmp_dir, file_name)
+    shutil.copyfile(src=os.path.join(os.getcwd(), file_name), dst=path)
+    add_execute_permission(path=Path(path))
+    os.chmod(path, 0o755)
+    return path
+
+
 def is_about_ps5(s: str) -> bool:
     return re.search(r"playstation5|ps5", s.lower()) is not None
 
 
-def run(driver_path: str) -> None:
-    driver = Chrome(driver_path)
+def run(driver_path: str, chromium_path: str) -> None:
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1280x1696")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--hide-scrollbars")
+    options.add_argument("--enable-logging")
+    options.add_argument("--log-level=0")
+    options.add_argument("--v=99")
+    options.add_argument("--single-process")
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--disable-dev-shm-usage")
+    options.binary_location = chromium_path
+    driver = Chrome(
+        service=Service(executable_path=driver_path),
+        options=options,
+    )
     driver.get(HIKARI_TV_URL)
     try:
         elements = driver.find_elements(By.XPATH, "//*[@class='recommendImg']")
@@ -33,18 +66,19 @@ def run(driver_path: str) -> None:
 
 
 def main(*args, **kwargs):
-    print("args:", args)
-    print("kwargs:", kwargs)
-    driver_file_name = "chromedriver"
-    with TemporaryDirectory() as tmp_dir:
-        tmp_driver_path = os.path.join(tmp_dir, driver_file_name)
-        shutil.copyfile(
-            src=os.path.join(os.getcwd(), driver_file_name),
-            dst=tmp_driver_path,
-        )
-        os.chmod(tmp_driver_path, 755)
-        run(driver_path=tmp_driver_path)
+    response = {"args": args, "kwargs": kwargs}
+    try:
+        with TemporaryDirectory() as tmp_dir:
+            tmp_driver_path = prepare_selenium_file(file_name="chromedriver", tmp_dir=tmp_dir)
+            tmp_chromium_path = prepare_selenium_file(file_name="headless-chromium", tmp_dir=tmp_dir)
+            response["tmp_driver_path"] = tmp_driver_path
+            response["tmp_chromium_path"] = tmp_chromium_path
+            run(driver_path=tmp_driver_path, chromium_path=tmp_chromium_path)
+    except Exception:
+        response["exception"] = format_exc()
+    finally:
+        return str(response)
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
